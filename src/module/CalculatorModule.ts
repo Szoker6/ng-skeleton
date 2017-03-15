e24dea0bdb04c8a2c8be5ff72dc5a2db85f6448b
mkuster <mkuster@ydp.eu>
2017-03-15 21:09:03 +0100

    Added FormsModule and jquery

--- src/module/CalculatorModule.ts
+++ src/module/CalculatorModule.ts
@@ -2,10 +2,12 @@ import '../../public/css/style.css';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {CalculatorComponent} from '../view/calculator.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   declarations: [
     CalculatorComponent
